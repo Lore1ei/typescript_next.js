ab3fbda6bb04d157b5ae5f5ba585f7289e72aa6e
Lore1ei <harubator@gmail.com>
2023-05-05 21:28:42 +0700

    navbar

--- src/components/NewsArticleEntry.tsx
+++ src/components/NewsArticleEntry.tsx
@@ -1,4 +1,3 @@
-import { FC } from 'react'
 import {NewsArticle} from "@/modules/NewsArticles";
 import {Card} from "react-bootstrap";
 import Image from "next/image";
@@ -8,28 +7,27 @@ interface NewsArticleEntryProps {
     article: NewsArticle
 }
 
-const NewsArticleEntry: FC = ({article : {title, description, url, urlToImage}}: NewsArticleEntryProps) => {
-    const validImageUrl = (urlToImage?.startsWith("http://") || urlToImage?.startsWith("https://")) ? urlToImage : undefined
+const NewsArticleEntry = ({ article: { title, description, url, urlToImage } }: NewsArticleEntryProps) => {
+
+    const validImageUrl = (urlToImage?.startsWith("http://") || urlToImage?.startsWith("https://")) ? urlToImage : undefined;
 
     return (
-        <>
-            <a href={url}>
-                <Card className="h-100">
-
-                    <Image
-                        src={validImageUrl || placeholderImage}
-                        alt=""
-                        width={500}
-                        height={200}
-                    />
-                    <Card.Body >
-                        <Card.Title>{title}</Card.Title>
-                        <Card.Text>{description}</Card.Text>
-                    </Card.Body>
-                </Card>
-            </a>
-        </>
-    )
+        <a href={url}>
+            <Card className="h-100">
+                <Image
+                    src={validImageUrl || placeholderImage}
+                    width={500}
+                    height={200}
+                    alt="News article image"
+                    className={`card-img-top`}
+                />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>{description}</Card.Text>
+                </Card.Body>
+            </Card>
+        </a>
+    );
 }
 
 export default NewsArticleEntry;
